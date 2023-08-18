7e066d4f771016087cd4a994d8a632c6d9eff997
Attila01 <48849467+Attila01@users.noreply.github.com>
2023-08-17 21:24:33 -0400

    Update php_install.sh
    phpMyAdmin est une interface web populaire pour la gestion de bases de données MySQL

--- php_install.sh
+++ php_install.sh
@@ -11,3 +11,27 @@ php -v
 
 # Activer le module PHP dans Apache
 sudo systemctl restart apache2
+
+# phpMyAdmin
+
+# Mettre à jour les informations des paquets
+sudo apt update
+
+# Installer phpMyAdmin et les dépendances
+sudo apt install phpmyadmin php-mbstring php-gettext
+
+# Sélectionnez Apache comme serveur web lors de la configuration et choisissez "yes" lorsque vous êtes invité à configurer la base de données avec dbconfig-common.
+
+# Activer l'extension PHP nécessaire pour phpMyAdmin
+sudo phpenmod mbstring
+
+# Redémarrer le serveur Apache
+sudo systemctl restart apache2
+
+# Ajouter phpMyAdmin au fichier de configuration Apache
+echo "Include /etc/phpmyadmin/apache.conf" | sudo tee -a /etc/apache2/apache2.conf
+
+# Redémarrer à nouveau le serveur Apache
+sudo systemctl restart apache2
+
+sudo systemctl restart mysql
